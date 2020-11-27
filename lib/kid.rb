05d5dfcddb27ee56f1684bb0b50f8afafc8061db
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-11-27 15:39:26 -0500

    commit tree 1222fad4453192dcd07aaf17fd1beab86eeccd8c

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,22 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+
+class Kid
+
+#We will include our fancy_dance nested modules in the Kid class. Using the keyword 
+#"include" allows us to share the modules methods as "instance" methods.
+#We use the keyword "extend" to share a modules methods as "class" methods
+#We use the nested modules by refering to them with the :: notation, which indicates the 
+#Parent::Child relationship of the nested modules 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end 
